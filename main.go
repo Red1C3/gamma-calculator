31e6261091390b5639313adae4652cc6bb9e8bb1
Mohammad Issawi <2000issawi@gmail.com>
2022-01-25 22:25:17 +0200

    Draw checkerboard next to testing area

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"image"
+	"image/color"
 	"log"
 
 	"golang.org/x/exp/shiny/driver"
@@ -13,7 +15,33 @@ const (
 	winHeight = 300
 )
 
-// TODO create a buffer and draw it
+var (
+	mainBuffer screen.Buffer
+)
+
+func fillMainBuffer(medGray uint8) {
+	startsWhite := true
+	for i := 0; i < mainBuffer.Size().X; i++ {
+		var div int
+		if startsWhite {
+			div = 1
+		} else {
+			div = 0
+		}
+		for j := 0; j < mainBuffer.Size().Y; j++ {
+			if i < mainBuffer.Size().X/2 {
+				if j%2 == div {
+					mainBuffer.RGBA().SetRGBA(i, j, color.RGBA{0, 0, 0, 255})
+				} else {
+					mainBuffer.RGBA().SetRGBA(i, j, color.RGBA{255, 255, 255, 255})
+				}
+			} else {
+				mainBuffer.RGBA().SetRGBA(i, j, color.RGBA{medGray, medGray, medGray, 255})
+			}
+		}
+		startsWhite = !startsWhite
+	}
+}
 func main() {
 	driver.Main(func(s screen.Screen) {
 		window, err := s.NewWindow(&screen.NewWindowOptions{Width: winWidth, Height: winHeight, Title: "gamma-calculator"})
@@ -21,6 +49,11 @@ func main() {
 			log.Fatal(err)
 		}
 		defer window.Release()
+		mainBuffer, err = s.NewBuffer(image.Point{winWidth, winHeight})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fillMainBuffer(128)
 		for {
 			switch e := window.NextEvent().(type) {
 			case lifecycle.Event:
@@ -28,6 +61,8 @@ func main() {
 					return
 				}
 			}
+			window.Upload(image.Pt(0, 0), mainBuffer, mainBuffer.Bounds())
+			window.Publish()
 		}
 	})
 }
